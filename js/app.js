c5fcbc194e5c86860177968552db330afa13b80c
Cem <cemarguvanli@gmail.com>
2015-05-12 14:25:02 +0300

    add set drag

--- js/app.js
+++ js/app.js
@@ -1,4 +1,35 @@
-$(document).ready(function() {
+
+window.addEventListener("load", setDrag, false);
+
+function setDrag(){
+	pic = document.getElementById("dexter");
+
+	// Step 1 prevent default
+	rightBox = document.getElementById("rightbox");
+	rightBox.addEventListener("dragenter", cancel, false);
+	rightBox.addEventListener("dragover", cancel, false);
 
 
-});
+
+	// Setup drag events
+	rightBox.addEventListener("dragstart", startDrag, false);
+	rightBox.addEventListener("drop", endDrag, false);
+}
+
+function startDrag(e){
+	// Store the content
+	var content = pic.innerHTML;
+	e.dataTransfer.setData('text/html', content);
+}
+
+function endDrag(e){
+	e.preventDefault();
+	this.innerHTML = e.dataTransfer.getData('text/html');
+}
+
+function cancel(e){
+	if (e.preventDefault) {
+		e.preventDefault();
+	}
+	return false;
+}
